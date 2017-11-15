3ce3762d545f25d3cb784ca88a0f2a709a2f0de5
mschier <mikkc13@student.sdu.dk>
2017-11-15 22:44:25 +0100

    Added a basic template for the keypad
    Figured out how to keep an altered QPushButton definition separate from the main code, but easy to call if needed.

--- Tests/minimal_keypad.py
+++ Tests/minimal_keypad.py
@@ -2,7 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt4 import QtGui
+from pyautogui import typewrite
+from PyQt4 import QtGui, QtCore
+
+class numKeyButton(QtGui.QPushButton):
+
+    def __init__(self, priKey, parent=None):
+        super(numKeyButton, self).__init__(parent)
+        self.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.setText(priKey)
+        self.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Expanding))
+        self.clicked.connect(lambda: typewrite(priKey))
 
 class Example(QtGui.QWidget):
     
@@ -21,26 +31,30 @@ class Example(QtGui.QWidget):
         grid = QtGui.QGridLayout()
         frame.setLayout(grid)
         
- 
+
+        line_ed = QtGui.QLineEdit(self)
+        line_ed.setFocus(True)
+        grid.addWidget(line_ed, 0,1, 1,2)
+
+        
         names = ['Cls', 'Bck', '', 'Close',
                  '7', '8', '9', '/',
                 '4', '5', '6', '*',
                  '1', '2', '3', '-',
                 '0', '.', '=', '+']
         
-        positions = [(i,j) for i in range(5) for j in range(4)]
+        positions = [(i,j) for i in range(1,5) for j in range(4)]
         
         for position, name in zip(positions, names):
             
             if name == '':
                 continue
             elif name == 'Close':
-                button = QtGui.QPushButton(name)
-                grid.addWidget(button, 0,2, 1,2)
+                button = numKeyButton(name)
+                grid.addWidget(button, 1,2, 1,2)
                 continue
             
-            button = QtGui.QPushButton(name)
-            button.setSizePolicy(QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Expanding))
+            button = numKeyButton(name)
             grid.addWidget(button, *position)
             
         self.move(300, 150)
